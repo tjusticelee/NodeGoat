322d206cd5e6c8d7148a0996ffaba726e6931c5a
Justice Lee <txawjcojlee@gmail.com>
2020-03-16 14:44:43 -0400

    cleaning up my commits

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ MongoClient.connect(config.db, function(err, db) {
         secret: config.cookieSecret,
         // Both mandatory in Express v4
         saveUninitialized: true,
-        resave: true
+        resave: true,
         /*
         // Fix for A5 - Security MisConfig
         // Use generic cookie name
@@ -94,10 +94,10 @@ MongoClient.connect(config.db, function(err, db) {
 
         // Fix for A3 - XSS
         // TODO: Add "maxAge"
-        cookie: {
+        cookie:{
             httpOnly: true
             // Remember to start an HTTPS server to get this working
-            secure: true
+          //  secure: true
         }
 
 
@@ -134,7 +134,7 @@ MongoClient.connect(config.db, function(err, db) {
     // Template system setup
     swig.setDefaults({
         // Autoescape disabled
-        autoescape: false
+        autoescape: false,
 
         // Fix for A3 - XSS, enable auto escaping
         autoescape: true // default value
